6ed6c7f5e8be72d67d11678523d6e167d92680c5
zhaozhentao <344696734@qq.com>
2019-08-17 09:44:07 +0800

    save

---
+++ src/util/avatars.js
@@ -0,0 +1,12 @@
+export const avatars = [
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg',
+  '../assets/avatar.jpeg'
+]
